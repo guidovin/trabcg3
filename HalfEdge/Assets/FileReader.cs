97407c33b8e4b3ff4ada86bc845b8bbab14dcb9a
Rodrigo Werneck <rodrigowfranco@gmail.com>
2016-07-29 04:35:17 -0300

    no message

---
+++ HalfEdge/Assets/FileReader.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Text;
+using System.IO;  
+using UnityEngine;
+using System.Collections;
+
+public class FileReader {
+
+	public string fileName;
+	public string[] entries;
+	public Polygon poly;
+
+		
+	private bool Load(string fileName)
+	{
+		// Tipo de leitura que o leitor está fazendo:
+		// false => vertices
+		// true  => half edges
+		bool readingType = false;
+		int[] nexts, twins;
+		nexts [0] = 0;
+		twins [0] = 0;
+
+		// Handle any problems that might arise when reading the text
+		try
+		{
+			string line;
+			// Create a new StreamReader, tell it which file to read and what encoding the file
+			// was saved as
+			StreamReader theReader = new StreamReader(fileName, Encoding.Default);
+			// Immediately clean up the reader after this block of code is done.
+			// You generally use the "using" statement for potentially memory-intensive objects
+			// instead of relying on garbage collection.
+			// (Do not confuse this with the using directive for namespace at the 
+			// beginning of a class!)
+			using (theReader)
+			{
+				do // WHILE there's lines left in the text file, do this:
+				{
+					line = theReader.ReadLine();
+
+					if (line == "#VERTICES"){
+						readingType = false;
+					}
+					else if(line == "#HALFEDGES") {
+						readingType = true;
+					}
+					else if (line != null)
+					{
+						string[] entries = line.Split(' '); //separa os valores na linha por espaço
+
+						if (entries.Length > 0)
+						{
+							int index = int.Parse(entries[0]);
+
+							if(!readingType) // trata vertices
+							{
+								
+								poly.vertices[index] = 
+									new Vertex(
+										float.Parse(entries[1]),
+										float.Parse(entries[2]), 
+										float.Parse(entries[3]) 
+									);
+							}
+							else //trata half edges
+							{
+								int vertexIndex = int.Parse(entries[1]);
+								int twinIndex  = int.Parse(entries[2]);
+								int nextIndex  = int.Parse(entries[3]);
+								int faceIndex = int.Parse(entries[4]);
+
+								HEdge hEdge = new HEdge();
+
+								hEdge.vert = poly.vertices[vertexIndex];
+
+								twins[index] = twinIndex;
+								nexts[index] = nextIndex;
+
+								if(poly.faces[faceIndex]) 
+									hEdge.face = poly.faces[faceIndex];
+								else
+									hEdge.face = poly.faces[faceIndex] = new Face(hEdge);
+
+								poly.vertices[vertexIndex].hEdge = 
+									poly.halfEdges[index] = 
+										hEdge;
+							}
+						}
+					}
+				}
+				while (line != null);
+
+				// Done reading, close the reader
+				theReader.Close();
+
+				for(int i = 1; i < twins.Length; i++)
+				{
+					poly.halfEdges[i].twin = poly.halfEdges[twins[i]];
+					poly.halfEdges[i].next = poly.halfEdges[nexts[i]];
+				}
+
+				return true;
+			}
+		}
+		catch (Exception e)
+		{
+		// If anything broke in the try block, we throw an exception with information
+		// on what didn't work
+
+			Console.WriteLine("{0}\n", e.Message);
+			return false;
+		}
+	}
+}
+}
---
+++ HalfEdge/Assets/HalfEdge.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Polygon {
+	public Vertex[] vertices;
+	public HEdge[] halfEdges;
+	public Face[] faces;
+}
+
+public class Vertex {
+
+	//Vars
+	public int index;
+	public float x, y, z;
+	public HEdge hEdge;
+
+	//Functions
+
+	//Constructor
+	public Vertex(float x, float y, float z){
+		this.x = x;		
+		this.y = y;		
+		this.z = z;
+	}
+
+	//Getter of vertex position on vector3 form
+	public Vector3 asVector()
+	{
+		return new Vector3 (x,y,z);
+	}
+}
+
+
+public class Face {
+
+	public int index;
+	public HEdge hEdge;
+
+	public Face(HEdge hEdge)
+	{
+		this.hEdge = hEdge;
+	}
+}
+
+public class HEdge {
+
+	//Vars --
+	public int index;
+	public Vertex vert; //origin vertex
+	public HEdge twin; //complementary pair
+	public HEdge next; //next
+	public Face face;
+
+	//Constructors --
+
+	public HEdge()
+	{
+	}
+
+	public HEdge(Vertex vert, HEdge twin, HEdge next, Face face)
+	{
+		this.vert = vert;
+		this.twin = twin;
+		this.next = next;
+		this.face = face;
+	}
+
+	// Esses são os exemplos mais simples q eu já fiz:
+
+	public Vertex[] getVertExtremes()
+	{
+		Vertex[] vertices;
+		vertices [0] = this.vert;
+		vertices [1] = this.next.vert;
+	}
+
+	public Face[] getFaces()
+	{
+		Face[] faces;
+		faces [0] = this.face;
+		faces [0] = this.twin.face;
+		return faces;
+	}
+
+
+	//Complete as funções a partir daqui (só no corpo das func, 
+	// não recebe parametro pq isso é método de instancia, ou seja vc quer encontrar os referentes ao "this") :
+
+	public Vertex[] getNeighborVertices()
+	{
+	}
+
+	public HEdge[] getNeighborHEdges()
+	{
+	}
+
+	public Face[] getNeighborFaces()
+	{
+	}
+}
